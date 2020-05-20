9cf2174287e17548a7b0182d40b54d0af9190ef7
selfedu-rus <65663799+selfedu-rus@users.noreply.github.com>
2020-05-20 17:30:45 +0400

    Initial commit

---
+++ UserLogin.py
@@ -0,0 +1,13 @@
+from flask_login import UserMixin
+
+class UserLogin(UserMixin):
+    def fromDB(self, user_id, db):
+        self.__user = db.getUser(user_id)
+        return self
+
+    def create(self, user):
+        self.__user = user
+        return self
+
+    def get_id(self):
+        return str(self.__user['id'])
